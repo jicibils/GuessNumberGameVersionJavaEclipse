e08b7cadf148b7e79a593d461665c7a7573c64fc
juan <jicibils@hotmail.com>
2018-05-03 12:15:57 -0300

    Title: Added the main class
    Body: the main class is called Main.java

---
+++ src/java/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner; //el prog utiliza la clase Scanner
+
+public class Main{
+  private static HumanBeing humanBeing = new HumanBeing();
+  private static Computer computer = new Computer();
+
+
+  public static void main( String[] args ){
+    System.out.println();
+    System.out.println("*******************************");
+    System.out.println();
+    System.out.println("Welcome to Guess Number Game!!!");
+    System.out.println();
+    System.out.println("*******************************");
+    System.out.println();
+    String response = "";
+    while (!(response.equals("end"))) {
+      System.out.println("Type 'me', if you want to guess a number. Otherwise, I will guess a number...");
+      System.out.print("Type 'end', if you want finish the game:");
+      Scanner input = new Scanner(System.in);
+      response = input.nextLine();
+      if (response.equals("me")) {
+        humanBeing.guess();
+      }
+      if ((!(response.equals("me")))&&(!(response.equals("end")))){
+        computer.guess();
+      }
+
+    }
+    System.out.println();
+    System.out.println("bye");
+
+  }
+}
